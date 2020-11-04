7d62293b9a1d2723e7bd6858c747b59cc852404b
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-11-04 11:55:08 -0800

    added components folder with header and photoOfDay files adding imports and exports with functions

--- src/App.js
+++ src/App.js
@@ -4,15 +4,48 @@ import React, { useState, useEffect} from "react";
 import "./App.css";
 //axios
 import axios from "axios"
+// import constants which is my base url and api
+import {BASE_URL, API_KEY} from "./constants/constants"
+//import from components file
+import Header from "./components/Header"
+import PhotoOfDay from "./components/"
 
 
 function App() {
+  // declaring the state
+  const [currentDate, setCurrentDate] = useState([]);
+  // declaring effect hook
+  useEffect(()=>{
+    const DailyPhoto = () => {
+      axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then((res) => {
+        console.log('res', res)
+        setCurrentDate(res.data)
+      })
+      .catch((err) => {
+        console.log('This is an error message', err)
+        debugger
+      })
+    }
+    console.log("daily", DailyPhoto());
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
+      <PhotoOfDay
+				date={currentDate.date}
+				title={currentDate.title}
+				imgSrc={currentDate.url}
+				imgAlt={currentDate.title}
+				description={currentDate.explanation}
+				copyright={currentDate.copyright}
+			/>
     </div>
   );
 }
---
+++ src/constants/PhotoOfDay.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+//pulling and exporting photos
+export default function PhotoOfDay(props) {
+    return(
+        <>
+        
+        </>
+    )
+}
