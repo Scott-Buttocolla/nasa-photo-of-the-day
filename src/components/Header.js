5cb2297654eb4f4d1ea8f0e586aaad5927c69e54
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-11-05 12:18:50 -0800

    day 2 styling added for header and photo, working on cleaning up styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Header() {
-	return <h1>NASA Photo of the Day</h1>;
+const StyledHeader = styled.div`
+	border: 1px solid black;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: space-between;
+	align-items: center;
+	margin: 20% 5%;
+	color: ${(pr) => pr.theme.danger};
+	.logo {
+		display:flex;
+		flex-direction:column ;
+		align-content:center;
+		border: 1px solid black;
+		max-width: 60%;
+	}
+	img{
+		margin:10%;
+	}
+
+	h1 {
+		font-size: 3rem;
+		font-weight: 700;
+		margin-bottom: 0.8rem;
+	}
+	h2 {
+		font-size: 2.4rem;
+		font-weight: 500;
+		margin-bottom: 0.5rem;
+	}
+	h3 {
+		font-size: 1.8rem;
+		font-weight: bolder;
+	}
+	@media ${(pr) => pr.theme.breakpoints.small} {
+		flex-flow: column wrap;
+		.logo {
+			margin-bottom: 2%;
+		}
+	}
+`;
+
+export default function Header(props) {
+	return (
+		<StyledHeader>
+			<div className='logo'>				
+				<img
+					src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/320px-NASA_Worm_logo.svg.png'
+					alt='NASA Logo'
+				/>				
+				<img src = 'https://fairport.org/wp-content/uploads/2017/12/nasa-logo.gif'
+				alt ="NASA Planetary Logo"
+				/>
+				</div>
+			<div>
+				<h1>Photo of the Day</h1>
+				<h2>Date:{props.date}</h2>
+				<h3>Title: {props.title}</h3>
+			</div>
+		</StyledHeader>
+	);
 }
--- src/components/PhotoOfDay.js
+++ src/components/PhotoOfDay.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledPhotoOfDay = styled.div`
+	display: flex;
+	flex-direction: column;
+    align-items: center;
+    justify-content: center;
+	border: 1px solid black;
+	//max-width: 100%;
+	margin: 0 auto;
+	img {
+		display:flex;
+		justify-content: center;
+		border: 5px solid red;
+		width: 60%;
+
+	}
+	p {
+		color: white;
+		background-color: #0e1726;
+		margin-top: 2%;
+		font-size: 1.6rem;
+		font-weight: 400;
+		line-height: 1.2;
+	}
+	.description {
+		text-align: left;
+	}
+	.copyright {
+		font-weight: 700;
+	}
+	@media ${(pr) => pr.theme.breakpoints.medium} {
+		width: 100%;
+		padding: 0 5%;
+	}
+`;
 
 export default function PhotoOfDay(props) {
 	return (
-		<>
-			<h2>{props.date}</h2>
-			<h3>{props.title}</h3>
+		<StyledPhotoOfDay>
 			<img src={props.imgSrc} alt={props.title} />
-			<p>{props.description}</p>
-			<p>© {props.copyright}</p>
-		</>
+			<p className='description'>{props.description}</p>
+			<p className='copyright'>© {props.copyright}</p>
+		</StyledPhotoOfDay>
 	);
 }
